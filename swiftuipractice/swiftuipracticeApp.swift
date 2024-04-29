06790e15023b66b7a8618cb91823e27f46961023
ad <ad>
2024-04-29 12:43:36 +0500

    Initial Commit

---
+++ swiftuipractice/swiftuipracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuipracticeApp.swift
+//  swiftuipractice
+//
+//  Created by Adlet Zhantassov on 29.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftuipracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
