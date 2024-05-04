511a4f84152938835ce849265acc36058fb95aa7
ad <ad>
2024-05-04 11:57:26 +0500

    lesson5

---
+++ swiftuipractice/OmodaCar_Lesson5/CarInfo.swift
@@ -0,0 +1,16 @@
+//
+//  CarInfo.swift
+//  swiftuipractice
+//
+//  Created by Adlet Zhantassov on 04.05.2024.
+//
+
+import Foundation
+
+struct CarInfo {
+    var carName: String
+    var carImageName: String
+    var engine: String
+    var carWheelDrive: String
+    var basePrice: Int
+}
